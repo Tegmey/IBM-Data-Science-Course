43a171ae966fd9bf78d1d284d6acee1cc4cac460
Tegmey <103416871+Tegmey@users.noreply.github.com>
2022-05-18 11:30:43 +0200

    Add files via upload

---
+++ Jupyter_Notebook.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <center>
+#     <img src="https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/Logos/organization_logo/organization_logo.png" width="300" alt="cognitiveclass.ai logo"  />
+# </center>
+# 
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# **Inga Tegetmeyer**  
+# Current occupation: Scientific research assistent
+
+# *I am interested in data science because I find it fascinating what kind of connections or insights can be gained by analyzing data properly.*
+
+# ##### The following code shows how to print "Hello World" in Python. 
+
+# In[5]:
+
+
+print("hello world")
+
+
+# > This is an example of a blockquote that is produced by using a > in front of the paragraph.
+# ---
+# 
+# 1. First line of a list with numbers
+# 2. This is the second line of a list with numbers.
+# 
+# ---
+# 
+# - This is the first line of a list with bullet points
+# - This is the second line of a list with bullet points.
+# 
+# ---
+# 
+# If you need any help, use https://www.google.de/. 
